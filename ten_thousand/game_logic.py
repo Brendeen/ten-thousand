7cf3f75d85cd3797ff7a95e035fe6dc193d90707
Brenden Moore <brendenm3603@gmail.com>
2023-04-04 22:20:07 -0700

    created method for game scoring logic

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -14,7 +14,38 @@ class GameLogic:
 
     @staticmethod
     def calculate_score(roll):
-        pass
+        lst = [0] * 6
+        roll_sum = 0
+        straight_counter = 0
+        doubles_counter = 0
+        triples_counter = 0
+
+        for die in roll:
+            lst[die - 1] += 1
+        for freq in range(6):
+            if lst[freq] > 2:
+                if (freq + 1) == 1:
+                    roll_sum += (lst[freq] - 2) * 1000
+                else:
+                    roll_sum += ((lst[freq] - 2) * (freq + 1) * 100)
+            elif (freq + 1) == 1:
+                roll_sum += (lst[freq] * 100)
+            elif (freq + 1) == 5:
+                roll_sum += (lst[freq] * 50)
+            if lst[freq] == 1:
+                straight_counter += 1
+            elif lst[freq] == 2:
+                doubles_counter += 1
+            elif lst[freq] == 3:
+                triples_counter += 1
+        if straight_counter == 6:
+            return 1500
+        elif doubles_counter == 3:
+            return 1500
+        elif triples_counter == 2:
+            return 1200
+
+        return roll_sum
 
 
 if __name__ == "__main__":
